ae4ccc93adf6d4b816d64d1149fa30bc5a39d628
Your NameNumark12 <fernandoerreca@gmail.com>
2022-04-27 00:27:02 -0300

    Agregados bloque general, banner y foto de perfil

--- TrabajoIntegrador_ErrecaFernando/src/app/app.module.ts
+++ TrabajoIntegrador_ErrecaFernando/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SocialComponent } from './components/social/social.component';
+import { MainComponent } from './components/main/main.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SocialComponent
+    SocialComponent,
+    MainComponent,
+    BannerComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
