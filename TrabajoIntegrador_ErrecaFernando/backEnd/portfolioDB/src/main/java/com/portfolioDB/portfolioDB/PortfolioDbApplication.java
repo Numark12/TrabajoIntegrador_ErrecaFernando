c1ee3da8b4133be374a9e8824480bad1f5853100
Your NameNumark12 <fernandoerreca@gmail.com>
2022-05-12 00:12:18 -0300

    con inicio del backend

---
+++ TrabajoIntegrador_ErrecaFernando/backEnd/portfolioDB/src/main/java/com/portfolioDB/portfolioDB/PortfolioDbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioDB.portfolioDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioDbApplication.class, args);
+	}
+
+}
---
+++ TrabajoIntegrador_ErrecaFernando/backEnd/portfolioDB/src/test/java/com/portfolioDB/portfolioDB/PortfolioDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioDB.portfolioDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
